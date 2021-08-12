efc5123fe54efc04020d53d9de79fc74f82a0a25
maliraza9 <maliraza260@gmail.com>
2021-08-12 15:57:43 +0200

    unified algorithm update 1

---
+++ UnifiedHeuristicsFunctions.jl
@@ -0,0 +1,39 @@
+function cableCrossingConstraint(x1,y1, x2,y2, x3,y3, x4,y4)
+    #statements
+    #statements
+    if crossed == true
+        return cableCrossing = true
+    else
+        return cableCrossing = false
+    end
+end
+
+function node2coordinate(positionMtx, visitedNodeSet, candNodeU, candNodeV)
+    #statements
+    #statements
+    return x1,y1, x2,y2, x3,y3, x4,y4
+end
+
+function capacityConstraint(visitedNodeV, capacityLimit)
+    #statements
+    #statements
+    if nodeConnections > turnbineLimit
+        return capExceed = true
+    else return capExceed = false
+    end
+end
+
+function loopCreation(visitedNodeSet, candNodeU, candNodeV)
+    #statements
+    #statements: loop exists if for candNodeU from disjoint set notVisitedNodeSet, the candNodeV exists in a disjoint set visitedNodeSet
+    if loopExists = true
+        return branchCycle = true
+    else branchCycle = false
+    end
+end
+
+function algorithm()
+    #statements
+    #statements
+    #statemetns
+end
